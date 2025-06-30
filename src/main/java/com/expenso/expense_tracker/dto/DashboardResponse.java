27f0fc0bacf9e6fe68d5cf0b4d52807295a6e3ae
AdityaRaj81 <Connect2RajAditya@gmail.com>
2025-06-30 14:14:40 +0530

    Enhance DashboardResponse to include recent transactions, monthly data, and category data lists

--- src/main/java/com/expenso/expense_tracker/dto/DashboardResponse.java
+++ src/main/java/com/expenso/expense_tracker/dto/DashboardResponse.java
@@ -1,8 +1,10 @@
 package com.expenso.expense_tracker.dto;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
 
 @Data
 @AllArgsConstructor
@@ -10,4 +12,7 @@ public class DashboardResponse {
     private double totalIncome;
     private double totalExpenses;
     private double balance;
+    private List<TransactionDTO> recentTransactions;
+    private List<MonthlyDataDTO> monthlyData;
+    private List<CategoryDataDTO> categoryData;
 }
