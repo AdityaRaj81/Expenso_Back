9e7a41ccb375e805d8aba1c6820efdb311623200
AdityaRaj81 <Connect2RajAditya@gmail.com>
2025-06-30 15:45:33 +0530

    Implement getAllTransactions method in TransactionService for user transaction retrieval

--- src/main/java/com/expenso/expense_tracker/service/TransactionService.java
+++ src/main/java/com/expenso/expense_tracker/service/TransactionService.java
@@ -1,17 +1,32 @@
 package com.expenso.expense_tracker.service;
 
-
 import com.expenso.expense_tracker.model.Transaction;
+import com.expenso.expense_tracker.dto.TransactionDTO;
 import com.expenso.expense_tracker.dto.TransactionRequest;
 import com.expenso.expense_tracker.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TransactionService {
+
+    public List<TransactionDTO> getAllTransactions(UUID userId) {
+    List<Transaction> transactions = transactionRepository.findByUserId(userId);
+
+    return transactions.stream().map(t -> new TransactionDTO(
+            String.valueOf(t.getId()),
+            t.getDescription(),
+            t.getAmount(),
+            t.getCategory(),
+            t.getType(),
+            t.getDate()
+    )).toList();
+}
+
 
     private final TransactionRepository transactionRepository;
 
