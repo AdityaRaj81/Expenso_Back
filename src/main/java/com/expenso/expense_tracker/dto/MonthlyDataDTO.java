6f6dfc8ff3d7ffe82e2246de30d097a634f43c2a
AdityaRaj81 <Connect2RajAditya@gmail.com>
2025-06-30 14:15:07 +0530

    Add MonthlyDataDTO class for monthly financial data representation

---
+++ src/main/java/com/expenso/expense_tracker/dto/MonthlyDataDTO.java
@@ -0,0 +1,12 @@
+package com.expenso.expense_tracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MonthlyDataDTO {
+    private String month;
+    private double income;
+    private double expenses;
+}
