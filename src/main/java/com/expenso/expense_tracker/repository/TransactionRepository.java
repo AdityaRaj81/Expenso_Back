768be2f70df082343573804d3469b9a4184cef72
AdityaRaj81 <Connect2RajAditya@gmail.com>
2025-06-30 17:14:39 +0530

    Add pagination support to findByUserId method in TransactionRepository

--- src/main/java/com/expenso/expense_tracker/repository/TransactionRepository.java
+++ src/main/java/com/expenso/expense_tracker/repository/TransactionRepository.java
@@ -7,6 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
     List<Transaction> findByUserId(UUID userID);
+
+    Page<Transaction> findByUserId(UUID userId, Pageable pageable); // ✅ added for pagination
 }
