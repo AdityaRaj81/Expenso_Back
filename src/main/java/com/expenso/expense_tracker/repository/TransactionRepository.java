5bcd2296943447bebf529c9be494ab5b7c61222f
AdityaRaj81 <Connect2RajAditya@gmail.com>
2025-07-04 01:52:07 +0530

    Add method to find transaction by ID and user ID in TransactionRepository

--- src/main/java/com/expenso/expense_tracker/repository/TransactionRepository.java
+++ src/main/java/com/expenso/expense_tracker/repository/TransactionRepository.java
@@ -1,17 +1,21 @@
 package com.expenso.expense_tracker.repository;
 
 import com.expenso.expense_tracker.model.Transaction;
-import com.expenso.expense_tracker.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-import java.util.UUID;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface TransactionRepository extends JpaRepository<Transaction, UUID>, JpaSpecificationExecutor<Transaction> {
     List<Transaction> findByUserId(UUID userID);
 
-    Page<Transaction> findByUserId(UUID userId, Pageable pageable); // ✅ added for pagination
+    Page<Transaction> findByUserId(UUID userId, Pageable pageable);
+
+    // ✅ Add this below line
+    Optional<Transaction> findByIdAndUserId(UUID id, UUID userId);
 }
